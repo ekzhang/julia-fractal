d9621b4eb05e4b03db02d1fbb06762a7396623a2
Eric Zhang <ekzhang1@gmail.com>
2017-10-07 13:45:17 -0500

    Histogram equalization

--- fractal.cpp
+++ fractal.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <chrono>
 #include <complex>
 #include <iostream>
@@ -25,9 +26,9 @@ std::tuple<int, int, int> palette(double x) {
     return std::make_tuple(r, g, b);
 }
 
-std::vector<unsigned char> julia_pixels(complex c, int width, int height,
-                                        int row_b, int row_e) {
-    std::vector<unsigned char> pix;
+std::vector<double> julia_pixels(complex c, int width, int height,
+                                 int row_b, int row_e) {
+    std::vector<double> pix;
     for (int i = row_b; i < row_e; i++) {
         for (int j = 0; j < width; j++) {
             double x = -1.4 + 2.8 * j / width;
@@ -42,13 +43,23 @@ std::vector<unsigned char> julia_pixels(complex c, int width, int height,
                 z = z * z + c;
             }
             smooth_color /= MAX_ITER;
-            auto color = palette(smooth_color);
-            pix.push_back(std::get<0>(color));
-            pix.push_back(std::get<1>(color));
-            pix.push_back(std::get<2>(color));
+            pix.push_back(smooth_color);
         }
     }
     return pix;
+}
+
+void equalize(std::vector<double>& img) {
+    std::vector<int> ordering(img.size());
+    for (int i = 0; i < img.size(); i++) {
+        ordering.push_back(i);
+    }
+    std::sort(ordering.begin(), ordering.end(), [&img](int i, int j) {
+        return img[i] < img[j];
+    });
+    for (int i = 0; i < ordering.size(); i++) {
+        img[ordering[i]] = 1.0 * i / ordering.size();
+    }
 }
 
 int main(int argc, char** argv) {
@@ -59,14 +70,14 @@ int main(int argc, char** argv) {
 
     // Julia Fractal function:
     // f(z) = z^2 - c
-    complex c; //(-0.221, -0.713);
+    complex c; // Ex: (-0.221, -0.713);
     std::cout << "Computing the Julia set for f(x) = x^2 - c; c = ";
     std::cin >> c;
-    
-    std::future<std::vector<unsigned char>> threads[NUM_THREADS];
 
     steady_clock::time_point start_time = steady_clock::now();
 
+    // Begin multithreaded Julia set calculations
+    std::future<std::vector<double>> threads[NUM_THREADS];
     for (int tid = 0; tid < NUM_THREADS; tid++) {
         int row_b = tid * HEIGHT / NUM_THREADS;
         int row_e = (tid + 1) * HEIGHT / NUM_THREADS;
@@ -74,17 +85,30 @@ int main(int argc, char** argv) {
                                   c, WIDTH, HEIGHT, row_b, row_e);
     }
 
-    std::vector<unsigned char> pix;
+    // Wait on the threads to finish, and combine results to form the image
+    std::vector<double> raw_colors;
     for (int tid = 0; tid < NUM_THREADS; tid++) {
         threads[tid].wait();
-        std::vector<unsigned char> partial = threads[tid].get();
-        pix.insert(pix.end(), partial.begin(), partial.end());
+        std::vector<double> partial = threads[tid].get();
+        raw_colors.insert(raw_colors.end(), partial.begin(), partial.end());
+    }
+
+    // Normalization procedure
+    equalize(raw_colors);
+
+    // Apply the color palette
+    std::vector<unsigned char> pix;
+    for (double d : raw_colors) {
+        auto t = palette(d);
+        pix.push_back(std::get<0>(t));
+        pix.push_back(std::get<1>(t));
+        pix.push_back(std::get<2>(t));
     }
     
     Image image;
     image.read(WIDTH, HEIGHT, "RGB", CharPixel, &pix[0]);
-    gaussianBlurImage blur(2, 0.4);
-    blur(image);
+    // gaussianBlurImage blur(2, 0.4);
+    // blur(image);
 
     steady_clock::time_point end_time = steady_clock::now();
     long long time_millis = duration_cast<milliseconds>(end_time - start_time).count();
